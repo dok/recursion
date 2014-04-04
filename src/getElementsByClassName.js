cdbb2ada76334701dec5feaa2dd86b1b32d29e7e
Sean Dokko <sean@hammerquist.net>
2014-04-04 14:58:16 -0700

    getElementByClassName finished

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,30 @@
 // };
 
 // But in stead we're going to implement it from scratch:
+// Document: a big tree
+// Base case: if at the end of the tree
+// Go through elements, if element has a class name match, then store that into an array, 
+// then observe children. 
 var getElementsByClassName = function (className) {
-  // your code here
+  // Return an array
+  var result = [];
+
+  var containsClass = function (node) {
+    return node.className.split(" ").indexOf(className) > -1 ? true : false;
+  };
+
+  var findMatches = function (child) {
+    if (child !== undefined) {
+      if ( containsClass(child) ) {
+        // console.log(child);
+        result.push(child);
+      }
+      for(var i = 0; i < child.children.length; i++) {
+        findMatches(child.children[i]);
+      }
+    }
+  };
+
+  findMatches(document.body);
+  return result;
 };
